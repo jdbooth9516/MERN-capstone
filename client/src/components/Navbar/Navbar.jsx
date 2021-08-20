3b5711f7998fdb45fe921c2b43feab71974a73c2
jdbooth9516 <jboothwebdev@gmail.com>
2021-08-20 11:18:19 -0700

    fix bug with the use in nav bar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -11,9 +11,14 @@ import {
 } from 'reactstrap';
 import '../../scss/Navbar.css';
 
-const NavBar = ({ user }) => {
+const NavBar = () => {
   const [collapsed, setCollapsed] = useState(true);
+  let user = 'none';
   let role = '';
+  if (document.cookie) {
+    let cookieCrumble = document.cookie.split('=');
+    user = cookieCrumble[1];
+  }
 
   const toggleNavbar = () => setCollapsed(!collapsed);
 
@@ -24,43 +29,43 @@ const NavBar = ({ user }) => {
 
   return (
     <div>
-      <Navbar className='navbar' color='faded' dark>
-        <NavbarBrand href='/' className='mr-auto'>
+      <Navbar className="navbar" color="faded" dark>
+        <NavbarBrand href="/" className="mr-auto">
           customKeys
         </NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className='mr-2' />
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
         <Collapse isOpen={!collapsed} navbar>
           <Nav navbar>
             {user === 'none' && (
               <div>
                 <NavItem>
-                  <NavLink href='/Register'>Register</NavLink>
+                  <NavLink href="/Register">Register</NavLink>
                 </NavItem>
 
                 <NavItem>
-                  <NavLink href='/login'>Login</NavLink>
+                  <NavLink href="/login">Login</NavLink>
                 </NavItem>
               </div>
             )}
             {role === 'employee' && (
               <div>
                 <NavItem>
-                  <NavLink href='/logout'>Logout</NavLink>
+                  <NavLink href="/logout">Logout</NavLink>
                 </NavItem>
 
                 <NavItem>
-                  <NavLink href='/portal'>Employee Portal</NavLink>
+                  <NavLink href="/portal">Employee Portal</NavLink>
                 </NavItem>
               </div>
             )}
             {role === 'customer' && (
               <div>
                 <NavItem>
-                  <NavLink href='/logout'>Logout</NavLink>
+                  <NavLink href="/logout">Logout</NavLink>
                 </NavItem>
 
                 <NavItem>
-                  <NavLink href='/cart'>Shopping Cart</NavLink>
+                  <NavLink href="/cart">Shopping Cart</NavLink>
                 </NavItem>
               </div>
             )}
